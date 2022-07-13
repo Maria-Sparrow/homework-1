b792e78f863a235fa8c27770705d6be2af5c465b
Maria-Sparrow <53764665+Maria-Sparrow@users.noreply.github.com>
2022-07-13 23:04:44 +0300

    Added som little fix

--- homework_10/homework10.js
+++ homework_10/homework10.js
@@ -6,6 +6,7 @@ function removeTransition(e) {
   function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
     if (!audio) return;
 
     key.classList.add('playing');
